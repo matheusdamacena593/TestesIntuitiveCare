d0e38f0007ffe315f0e382d6c3af1e0eae055e3f
matheusdamacena593 <mateusdamacena593@gmail.com>
2025-03-28 21:49:08 -0300

    Atualizar consultas SQL e adicionar coleção Postman para busca de registros

--- Teste 3/query_analitica.sql
+++ Teste 3/query_analitica.sql
@@ -12,7 +12,8 @@ SELECT op.razao_social, SUM(rt.vl_saldo_final - rt.vl_saldo_inicial) AS despesa_
 FROM operadoras_ativas op
 INNER JOIN registros_trimestrais rt ON op.registro_ans = rt.reg_ans
 WHERE rt.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
-AND rt.data >= DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 year'
+AND rt.data >= '2024-01-01'
+AND rt.data < '2025-01-01'
 GROUP BY op.razao_social
 ORDER BY despesa_total DESC
 LIMIT 10;
